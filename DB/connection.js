a4e6913abd367f8bd9d8ef4b78e7251bc54234e9
Trupti@123 <trupti@cyperts.net>
2024-08-25 22:21:42 +0530

    finalised project

--- DB/connection.js
+++ DB/connection.js
@@ -1,15 +1,15 @@
 const { MongoClient } = require('mongodb');
 
-// Replace with your MongoDB connection string
+
 const uri =process.env.DATABASE;
 
-const client = new MongoClient(uri); // No need for useNewUrlParser and useUnifiedTopology options
+const client = new MongoClient(uri); 
 
 async function connectToDatabase() {
   try {
     await client.connect();
     console.log("Connected to MongoDB");
-    return client.db('your_database_name'); // Replace with your database name
+    return client.db('your_database_name');
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
     process.exit(1);
